3b55681c5d0dbd5cc65b55bce75e00a6f0dc22da
amuqshit000 <muqshit.cse@gmail.com>
2024-02-23 22:58:45 +0600

    some

--- script/rectangle-calculate.js
+++ script/rectangle-calculate.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-    
+    //get length
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLength = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLength);
+    console.log(length);
+
+    //get height
+    const rectangleHeightInput = document.getElementById('rectangle-height');
+    const rectangleHeight = rectangleHeightInput.value;
+    const height = parseFloat(rectangleHeight);
+    console.log(height);
+
+    //area
+    const area = length * height;
+    console.log(area);
+
+    //display rectangle-area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
